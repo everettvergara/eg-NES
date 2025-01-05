2505c88a0befca5d1cdc49ee4f5d13f180d28b6d
Everett Gaius Vergara <evergara@shinra.com.ph>
2025-01-05 17:17:35 +0800

    misc edits

---
+++ egNES1/egNES1.cpp
@@ -0,0 +1,14 @@
+// References:
+// https://www.c64-wiki.com
+// https://www.masswerk.at/6502/6502_instruction_set.html
+
+#include <iostream>
+#include "6502.h"
+
+int main()
+{
+	eg::generic_6502 test;
+	test.reset();
+
+	return 0;
+}
