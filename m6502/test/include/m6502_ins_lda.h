bf68147e8e8ebcb2b00cad8e639ad64f5b0c8bb5
Everett Gaius Vergara <evergara@shinra.com.ph>
2025-01-19 19:19:44 +0800

    code refactor

--- m6502/test/include/m6502_ins_lda.h
+++ m6502/test/include/m6502_ins_lda.h
@@ -1,20 +1,26 @@
 #pragma once
 
 #include "types.h"
+#include "reg.h"
 
 namespace eg::m6502
 {
 	inline constexpr word RESET_VECTOR_ADDRESS = 0xfffc;
 	inline constexpr word RESET_ROUTINE_ADDRESS = 0x0000;
 
+	auto test_LDA_reg_check(const reg& t, byte val) -> bool;
+
+	auto test_LDA_IM(byte val) -> bool;
+	auto test_LDA_ZP(byte val) -> bool;
+	auto test_LDA_ZPX(byte val) -> bool;
+	auto test_LDA_ABS(byte val) -> bool;
+
 	auto test_LDA_IM_nzero_nneg() -> bool;
 	auto test_LDA_IM_zero_nneg() -> bool;
 	auto test_LDA_IM_nzero_neg() -> bool;
-
 	auto test_LDA_ZP_nzero_nneg() -> bool;
 	auto test_LDA_ZP_zero_nneg() -> bool;
 	auto test_LDA_ZP_nzero_neg() -> bool;
-
 	auto test_LDA_ZPX_nzero_nneg() -> bool;
 	auto test_LDA_ZPX_zero_nneg() -> bool;
 	auto test_LDA_ZPX_nzero_neg() -> bool;
--- m6502/test/src/m6502_ins_lda.cpp
+++ m6502/test/src/m6502_ins_lda.cpp
@@ -3,7 +3,14 @@
 
 namespace eg::m6502
 {
-	auto test_LDA_IM_nzero_nneg() -> bool
+	auto test_LDA_reg_check(const reg& t, byte val) -> bool
+	{
+		return t.AC == val and
+			t.SR.Z == (val == 0 ? 1 : 0) and
+			t.SR.N == (val & 0b10000000) > 0 ? 1 : 0;
+	}
+
+	auto test_LDA_IM(byte val) -> bool
 	{
 		m6502_generic cpu(RESET_VECTOR_ADDRESS, RESET_ROUTINE_ADDRESS);
 
@@ -11,72 +18,15 @@ namespace eg::m6502
 
 		mem data;
 		data[RESET_VECTOR_ADDRESS] = LDA_IM;
-		data[RESET_VECTOR_ADDRESS + 1] = 'A';
+		data[RESET_VECTOR_ADDRESS + 1] = val;
 
 		cpu.test_load_mem(std::move(data));
 		cpu.exec();
 
-		if (const reg t = cpu.get_reg();
-
-			t.AC == 'A' and
-			t.SR.Z == 0 and
-			t.SR.N == 0)
-		{
-			return true;
-		}
-
-		return false;
+		return test_LDA_reg_check(cpu.get_reg(), val);
 	}
 
-	auto test_LDA_IM_zero_nneg() -> bool
-	{
-		m6502_generic cpu(RESET_VECTOR_ADDRESS, RESET_ROUTINE_ADDRESS);
-
-		cpu.reset();
-
-		mem data;
-		data[RESET_VECTOR_ADDRESS] = LDA_IM;
-		data[RESET_VECTOR_ADDRESS + 1] = 0;
-
-		cpu.test_load_mem(std::move(data));
-		cpu.exec();
-
-		if (const reg t = cpu.get_reg();
-
-			t.AC == 0 and
-			t.SR.Z == 1 and
-			t.SR.N == 0)
-		{
-			return true;
-		}
-
-		return false;
-	}
-
-	auto test_LDA_IM_nzero_neg() -> bool
-	{
-		m6502_generic cpu(RESET_VECTOR_ADDRESS, RESET_ROUTINE_ADDRESS);
-		cpu.reset();
-
-		mem data;
-		data[RESET_VECTOR_ADDRESS] = LDA_IM;
-		data[RESET_VECTOR_ADDRESS + 1] = 235;
-
-		cpu.test_load_mem(std::move(data));
-		cpu.exec();
-
-		if (const reg t = cpu.get_reg();
-			t.AC == 235 and
-			t.SR.Z == 0 and
-			t.SR.N == 1)
-		{
-			return true;
-		}
-
-		return false;
-	}
-
-	auto test_LDA_ZP_nzero_nneg() -> bool
+	auto test_LDA_ZP(byte val) -> bool
 	{
 		m6502_generic cpu(RESET_VECTOR_ADDRESS, RESET_ROUTINE_ADDRESS);
 
@@ -85,74 +35,15 @@ namespace eg::m6502
 		mem data;
 		data[RESET_VECTOR_ADDRESS] = LDA_ZP;
 		data[RESET_VECTOR_ADDRESS + 1] = 0x05;
-		data[0x0005] = 'A';
+		data[0x0005] = val;
 
 		cpu.test_load_mem(std::move(data));
 		cpu.exec();
 
-		if (const reg t = cpu.get_reg();
-
-			t.AC == 'A' and
-			t.SR.Z == 0 and
-			t.SR.N == 0)
-		{
-			return true;
-		}
-
-		return false;
+		return test_LDA_reg_check(cpu.get_reg(), val);
 	}
 
-	auto test_LDA_ZP_zero_nneg() -> bool
-	{
-		m6502_generic cpu(RESET_VECTOR_ADDRESS, RESET_ROUTINE_ADDRESS);
-
-		cpu.reset();
-
-		mem data;
-		data[RESET_VECTOR_ADDRESS] = LDA_ZP;
-		data[RESET_VECTOR_ADDRESS + 1] = 0x05;
-		data[0x0005] = 0;
-
-		cpu.test_load_mem(std::move(data));
-		cpu.exec();
-
-		if (const reg t = cpu.get_reg();
-
-			t.AC == 0 and
-			t.SR.Z == 1 and
-			t.SR.N == 0)
-		{
-			return true;
-		}
-
-		return false;
-	}
-
-	auto test_LDA_ZP_nzero_neg() -> bool
-	{
-		m6502_generic cpu(RESET_VECTOR_ADDRESS, RESET_ROUTINE_ADDRESS);
-		cpu.reset();
-
-		mem data;
-		data[RESET_VECTOR_ADDRESS] = LDA_ZP;
-		data[RESET_VECTOR_ADDRESS + 1] = 0x05;
-		data[0x0005] = 235;
-
-		cpu.test_load_mem(std::move(data));
-		cpu.exec();
-
-		if (const reg t = cpu.get_reg();
-			t.AC == 235 and
-			t.SR.Z == 0 and
-			t.SR.N == 1)
-		{
-			return true;
-		}
-
-		return false;
-	}
-
-	auto test_LDA_ZPX_nzero_nneg() -> bool
+	auto test_LDA_ZPX(byte val) -> bool
 	{
 		m6502_generic cpu(RESET_VECTOR_ADDRESS, RESET_ROUTINE_ADDRESS);
 
@@ -162,79 +53,16 @@ namespace eg::m6502
 
 		data[RESET_VECTOR_ADDRESS] = LDA_ZPX;
 		data[RESET_VECTOR_ADDRESS + 1] = 0x05;
-		data[0x00f5] = 'A';
+		data[0x00f5] = val;
 
 		cpu.test_load_mem(std::move(data));
 		cpu.test_load_reg_X(0xf0);
 		cpu.exec();
 
-		if (const reg t = cpu.get_reg();
-
-			t.AC == 'A' and
-			t.SR.Z == 0 and
-			t.SR.N == 0)
-		{
-			return true;
-		}
-
-		return false;
+		return test_LDA_reg_check(cpu.get_reg(), val);
 	}
 
-	auto test_LDA_ZPX_zero_nneg() -> bool
-	{
-		m6502_generic cpu(RESET_VECTOR_ADDRESS, RESET_ROUTINE_ADDRESS);
-
-		cpu.reset();
-
-		mem data;
-
-		data[RESET_VECTOR_ADDRESS] = LDA_ZPX;
-		data[RESET_VECTOR_ADDRESS + 1] = 0x05;
-		data[0x00f5] = 0;
-
-		cpu.test_load_mem(std::move(data));
-		cpu.test_load_reg_X(0xf0);
-		cpu.exec();
-
-		if (const reg t = cpu.get_reg();
-
-			t.AC == 0 and
-			t.SR.Z == 1 and
-			t.SR.N == 0)
-		{
-			return true;
-		}
-
-		return false;
-	}
-
-	auto test_LDA_ZPX_nzero_neg() -> bool
-	{
-		m6502_generic cpu(RESET_VECTOR_ADDRESS, RESET_ROUTINE_ADDRESS);
-		cpu.reset();
-
-		mem data;
-
-		data[RESET_VECTOR_ADDRESS] = LDA_ZPX;
-		data[RESET_VECTOR_ADDRESS + 1] = 0x05;
-		data[0x00f5] = 235;
-
-		cpu.test_load_mem(std::move(data));
-		cpu.test_load_reg_X(0xf0);
-		cpu.exec();
-
-		if (const reg t = cpu.get_reg();
-			t.AC == 235 and
-			t.SR.Z == 0 and
-			t.SR.N == 1)
-		{
-			return true;
-		}
-
-		return false;
-	}
-
-	auto test_LDA_ABS_nzero_nneg() -> bool
+	auto test_LDA_ABS(byte val) -> bool
 	{
 		m6502_generic cpu(RESET_VECTOR_ADDRESS, RESET_ROUTINE_ADDRESS);
 
@@ -245,75 +73,24 @@ namespace eg::m6502
 		data[RESET_VECTOR_ADDRESS] = LDA_ABS;
 		data[RESET_VECTOR_ADDRESS + 1] = 0x02;
 		data[RESET_VECTOR_ADDRESS + 2] = 0x01;
-		data[0x0102] = 'A';
+		data[0x0102] = val;
 
 		cpu.test_load_mem(std::move(data));
 		cpu.exec();
 
-		if (const reg t = cpu.get_reg();
-
-			t.AC == 'A' and
-			t.SR.Z == 0 and
-			t.SR.N == 0)
-		{
-			return true;
-		}
-
-		return false;
+		return test_LDA_reg_check(cpu.get_reg(), val);
 	}
 
-	auto test_LDA_ABS_zero_nneg() -> bool
-	{
-		m6502_generic cpu(RESET_VECTOR_ADDRESS, RESET_ROUTINE_ADDRESS);
-
-		cpu.reset();
-
-		mem data;
-
-		data[RESET_VECTOR_ADDRESS] = LDA_ABS;
-		data[RESET_VECTOR_ADDRESS + 1] = 0x02;
-		data[RESET_VECTOR_ADDRESS + 2] = 0x01;
-		data[0x0102] = 0;
-
-		cpu.test_load_mem(std::move(data));
-
-		cpu.exec();
-
-		if (const reg t = cpu.get_reg();
-
-			t.AC == 0 and
-			t.SR.Z == 1 and
-			t.SR.N == 0)
-		{
-			return true;
-		}
-
-		return false;
-	}
-
-	auto test_LDA_ABS_nzero_neg() -> bool
-	{
-		m6502_generic cpu(RESET_VECTOR_ADDRESS, RESET_ROUTINE_ADDRESS);
-		cpu.reset();
-
-		mem data;
-
-		data[RESET_VECTOR_ADDRESS] = LDA_ABS;
-		data[RESET_VECTOR_ADDRESS + 1] = 0x02;
-		data[RESET_VECTOR_ADDRESS + 2] = 0x01;
-		data[0x0102] = 235;
-
-		cpu.test_load_mem(std::move(data));
-		cpu.exec();
-
-		if (const reg t = cpu.get_reg();
-			t.AC == 235 and
-			t.SR.Z == 0 and
-			t.SR.N == 1)
-		{
-			return true;
-		}
-
-		return false;
-	}
+	auto test_LDA_IM_nzero_nneg() -> bool { return test_LDA_IM('A'); }
+	auto test_LDA_IM_zero_nneg() -> bool { return test_LDA_IM(0); }
+	auto test_LDA_IM_nzero_neg() -> bool { return test_LDA_IM(235); }
+	auto test_LDA_ZP_nzero_nneg() -> bool { return test_LDA_ZP('A'); }
+	auto test_LDA_ZP_zero_nneg() -> bool { return test_LDA_ZP(0); }
+	auto test_LDA_ZP_nzero_neg() -> bool { return test_LDA_ZP(235); }
+	auto test_LDA_ZPX_nzero_nneg() -> bool { return test_LDA_ZPX('A'); }
+	auto test_LDA_ZPX_zero_nneg() -> bool { return test_LDA_ZPX(0); }
+	auto test_LDA_ZPX_nzero_neg() -> bool { return test_LDA_ZPX(235); }
+	auto test_LDA_ABS_nzero_nneg() -> bool { return test_LDA_ABS('A'); }
+	auto test_LDA_ABS_zero_nneg() -> bool { return test_LDA_ABS(0); }
+	auto test_LDA_ABS_nzero_neg() -> bool { return test_LDA_ABS(235); }
 }
